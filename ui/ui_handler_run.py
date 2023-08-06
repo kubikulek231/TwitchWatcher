e5911a33b1f0c75b71dfac0603b0cf7e2f9cced2
unknown <kuba.lepik@seznam.cz>
2023-08-06 23:52:38 +0200

    moved from pynput to keyboard

--- ui/ui_handler_run.py
+++ ui/ui_handler_run.py
@@ -1,6 +1,6 @@
 import time
 
-from pynput import keyboard
+import keyboard
 
 from ui.ui_handler_main import UIHandlerMain, UICleaner
 from ui.ui_handler_run_data import UIDataManager
@@ -33,18 +33,17 @@ class UIHandlerRun:
         except Exception as e:
             print(e)
 
-    def _on_key_release(self, key) -> None:
-        if key == keyboard.Key.esc and not self._stopping:
+    def _on_key_release(self, event: keyboard.KeyboardEvent) -> None:
+        if event.name == 'esc' and not self._stopping:
             self._running = False
             self._stopping = True
             print()
             print(f" {UIDataManager.get_current_time_string()} @User: watcher STOP requested")
 
     def run(self) -> None:
-        listener = keyboard.Listener(on_release=self._on_key_release)
-        listener.start()
+        listener = keyboard.on_release(callback=self._on_key_release)
         self._watcher_working()
-        listener.stop()
+        keyboard.unhook(listener)
         print()
         print(f" {UIDataManager.get_current_time_string()} @User: watcher STOPPED successfully")
         print()

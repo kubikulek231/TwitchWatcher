fd2f04e9c62abb230c869a138c62e4f5f0252429
unknown <kuba.lepik@seznam.cz>
2023-08-07 15:17:17 +0200

    chromedriver path fixed?

--- handler/browser_handler.py
+++ handler/browser_handler.py
@@ -52,10 +52,10 @@ class BrowserHandler:
             if os.name == "nt":  # Windows
                 driver_path = "chromedriver/chromedriver.exe"
             else:  # Assuming other OS (Linux, macOS, etc.)
-                driver_path = "./chromedriver/chromedriver"
+                driver_path = "chromedriver/chromedriver"
 
             self._driver = uc.Chrome(headless=headless, use_subprocess=False, options=chrome_options,
-                                     driver_executable_path=f"{driver_path}")
+                                     driver_executable_path=driver_path)
             return True
         return False
 
